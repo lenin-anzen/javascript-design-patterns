9c87d0e3d6b36bcfd4ca8f99d42ae901105f17a6
Lenin Meza <merolhack@gmail.com>
2017-06-13 13:38:10 -0500

    Observer pattern

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Javascript Design Patterns
 Patrones de diseño en Javascript
+
+[GitHub page](https://lenin-anzen.github.io/javascript-design-patterns/)
+
+#### Immediately Invoked Function Expressions (IIFE)
+```javascript
+(function() {
+    var foo = 'bar';
+    console.log('Foo value:', foo);
+})();
+```
---
+++ docs/assets/js/observer.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var Subject = function() {
+    var observers = [];
+    return {
+        subscribeObserver: function(observer) {
+            observers.push(observer);
+        },
+        unsubscribeObserver: function(observer) {
+            var index = observers.indexOf(observer);
+            if (index > -1) {
+                observers.splice(index, 1);
+            }
+        },
+        notifyObserver: function(observer) {
+            var index = observers.indexOf(observer);
+            if (index > -1) {
+                return observers[index].notify(index);
+            }
+        },
+        notifyAllObservers: function() {
+            var notifications = [];
+            for (var i = 0; i < observers.length; i++) {
+                notifications.push(observers[i].notify(i));
+            };
+            return notifications;
+        }
+    };
+};
+var Observer = function(value) {
+    var number = value;
+    return {
+        notify: function(index) {
+            return "Observer "+number+" ("+index+") is notified!";
+        }
+    }
+}
+var subject = new Subject();
+
+var observer1 = new Observer(1);
+var observer2 = new Observer(2);
+var observer3 = new Observer(3);
+var observer4 = new Observer(4);
+
+subject.subscribeObserver(observer1);
+subject.subscribeObserver(observer2);
+subject.subscribeObserver(observer3);
+subject.subscribeObserver(observer4);
+
+var notifySecondObserver = function() {
+    document.getElementById("js-observer-notifysecondobserver").innerHTML = subject.notifyObserver(observer2); // Observer 2 is notified!
+}
+var notifyAll = function() {
+    var notifications = subject.notifyAllObservers();
+    for ( var i in notifications ) {
+        var elem = document.createElement('p');
+        elem.innerHTML = notifications[i];
+        document.getElementById("js-observer-notifyall").appendChild(elem);
+    }
+    // Observer 1 is notified!
+    // Observer 2 is notified!
+    // Observer 3 is notified!
+    // Observer 4 is notified!
+}
